f37020312a7a65f6d9c1fa6d0cfbe51470516585
ArthurOliCode <arthur.rilber@gmail.com>
2026-03-12 08:52:26 -0300

    Estabelecendo o mapeamento da classe principal

--- produtosapi/produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
+++ produtosapi/produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
@@ -1,9 +1,25 @@
 package com.example.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // Indica que a classe está relacionada a uma tabela do banco, e que as instâncias dela correspondem às linhas dessa tabela.
+@Table(name = "produto") // Indica o nome da tabela do banco, ao qual essa entidade se refere.
 public class Produto {
+
+    @Id // Transforma em Id o campo --> sinalizando a primary Key.
+    @Column(name = "id") // Indica o nome ao qual esse campo se refere na tabela, além das propriedades também.
     private String id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public String getId() {
